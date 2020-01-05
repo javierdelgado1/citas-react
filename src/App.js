b3cae5090d1ba5264d8960907a86cde69da28bd1
Javier <ingeniero@javierdelgado.com.ve>
2020-01-05 16:27:18 -0300

    agregando state al formulario

--- src/App.js
+++ src/App.js
@@ -1,50 +1,67 @@
 import React, {useState, Fragment} from 'react';
 
 function Formulario (){
+  const [cita, actualizarCita] = useState({
+    mascota:'',
+    propietario:'',
+    fecha:'',
+    hora:'',
+    sintomas:''
+  });
+  const actualizarState = e =>{
+    actualizarCita({
+      ...cita,
+      [e.target.name] :e.target.value
+    });
+  }
   return (
     <Fragment>
-      <h2>Crear Cita</h2>
+        <h2>Crear Cita</h2>
+            <form>
+              <label>Nombre Mascota</label>
+              <input 
+                type="text" 
+                name="mascota"
+                className="u-full-width" 
+                placeholder="Nombre Mascota" 
+                onChange={actualizarState}
+              />
 
-      <form>
-                  <label>Nombre Mascota</label>
-                  <input 
-                    type="text" 
-                    name="mascota"
-                    className="u-full-width" 
-                    placeholder="Nombre Mascota" 
-                  />
+              <label>Nombre Dueño</label>
+              <input 
+                type="text" 
+                name="propietario"
+                className="u-full-width"  
+                placeholder="Nombre Dueño de la Mascota" 
+                onChange={actualizarState}
+              />
 
-                  <label>Nombre Dueño</label>
-                  <input 
-                    type="text" 
-                    name="propietario"
-                    className="u-full-width"  
-                    placeholder="Nombre Dueño de la Mascota" 
-                  />
+              <label>Fecha</label>
+              <input 
+                type="date" 
+                className="u-full-width"
+                name="fecha"
+                onChange={actualizarState}
+              />               
 
-                  <label>Fecha</label>
-                  <input 
-                    type="date" 
-                    className="u-full-width"
-                    name="fecha"
-                  />               
+              <label>Hora</label>
+              <input 
+                type="time" 
+                className="u-full-width"
+                name="hora" 
+                onChange={actualizarState}
+              />
 
-                  <label>Hora</label>
-                  <input 
-                    type="time" 
-                    className="u-full-width"
-                    name="hora" 
-                  />
+              <label>Sintomas</label>
+              <textarea 
+                className="u-full-width"
+                name="sintomas"
+                onChange={actualizarState}
+              ></textarea>
 
-                  <label>Sintomas</label>
-                  <textarea 
-                    className="u-full-width"
-                    name="sintomas"
-                  ></textarea>
-
-                  <button type="submit" className="button-primary u-full-width">Agregar</button>
-          </form>
-  </Fragment>
+              <button type="submit" className="button-primary u-full-width">Agregar</button>
+            </form>
+    </Fragment>
   ); 
 }
 function App() {
